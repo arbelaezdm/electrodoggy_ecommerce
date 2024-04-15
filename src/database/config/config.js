e081890e7fdbe1656940e3111412880db63e0f26
Diego Arbelaez <arbelaez.dm@hotmail.com>
2024-04-14 20:04:29 -0400

    fixing database

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,25 @@
+require('dotenv').config()
+
 module.exports ={
   "development": {
-    "username": "root",
-    "password": '8127',
-    "database": "electrodoggy_db",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER_NAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "electrodoggy_db",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER_NAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "electrodoggy_db",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER_NAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   }
 }
